ef067fe09c09331b7534030d42b0e70bd916480f
Krueger <33267349+kruegeco@users.noreply.github.com>
2018-04-03 18:48:48 -0400

    error fix
    pushed via large copy/paste

--- src/main.cpp
+++ src/main.cpp
@@ -2,39 +2,46 @@
 #include <stdio.h>
 #include "sorts.h"
 #include "celebrity.hpp"
+
 //argv[0] = program name
 //argv[1] = his sort
 //argv[2] = how many to make
 int main(int argc, char** argv) {
-	std::vector<Celebrity*> celebs;
+	std::vector<Celebrity> celebs;
 	int sort = std::stoi(argv[1]);
 	int size = std::stoi(argv[2]);
 	for (int i = 0; i < size; i++) {
-		celebs.vector::push_back(new Celebrity());
-		std::cout << celebs[i]->name << std::endl;
-		std::cout << celebs[i]->howBadly << std::endl;
+	  	Celebrity* c1 =  new Celebrity();
+		celebs.vector::push_back(*c1);
+		//std::cout << celebs[i].name << std::endl;
+		//std::cout << celebs[i].howBadly << std::endl;
 	}
 	std::cout << "the sort used is " << sort;
 	switch(sort) {
 		case 1:
 			std::cout << ": bubble Sort!" << std::endl;
 			bubble_sort(celebs);
+			break;
 		case 2:
-			std::cout << ": bubble Sort!" << std::endl;
-			
+			std::cout << ": selection Sort!" << std::endl;
+			selection_sort(celebs);
+			break;
 		case 3:
-			std::cout << ": bubble Sort!" << std::endl;
-			
+			std::cout << ": insertion Sort!" << std::endl;
+			insertion_sort(celebs);
+			break;
 		case 4:
-			std::cout << ": bubble Sort!" << std::endl;
-			
+			std::cout << ": merge Sort!" << std::endl;
+			mergeSort(celebs);
+			break;
 		case 5:
-			std::cout << ": bubble Sort!" << std::endl;
-			
+			std::cout << ": quick Sort!" << std::endl;
+			//quicksort(celebs);
+			break;
 	}
 	for (int i = 0; i < size; i++) {
-		std::cout << celebs[i]->name << std::endl;
-		std::cout << celebs[i]->howBadly << std::endl;
+		//std::cout << celebs[i].name << std::endl;
+		std::cout << celebs[i].howBadly << std::endl;
 	}
 	return 0;
 }
